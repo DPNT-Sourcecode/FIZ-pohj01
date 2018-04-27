3fd6c65d14c5615e5ec9d70221313bc3fd779119
sbx_user1059 <sbx_user1059@ip-10-12-7-208.eu-west-2.compute.internal>
2018-04-27 10:32:57 +0000

    Fri Apr 27 10:32:57 UTC 2018

--- src/BeFaster.App.Tests/Solutions/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SumSolutionTest.cs
@@ -11,5 +11,29 @@ namespace BeFaster.App.Tests.Solutions
         {
             return SumSolution.Sum(x, y);
         }
+
+        [TestCase(-1, 5, ExpectedResult = -1)]
+        public int ComputeSum_X_invalidrange_lower(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+        [TestCase(101, 5, ExpectedResult = -1)]
+        public int ComputeSum_X_invalidrange_higher(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+        [TestCase(5, -1, ExpectedResult = -1)]
+        public int ComputeSum_Y_invalidrange_lower(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+        [TestCase(5, 101, ExpectedResult = -1)]
+        public int ComputeSum_Y_invalidrange_higher(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
     }
 }
