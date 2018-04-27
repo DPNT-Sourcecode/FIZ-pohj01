96e412734c9d2b0bd789efdf41dd1d06096e417c
sbx_user1060 <sbx_user1060@ip-10-11-203-51.eu-west-2.compute.internal>
2018-04-27 12:39:50 +0000

    Fri Apr 27 12:39:50 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -4,6 +4,9 @@
     {
         public static string FizzBuzz(int number)
         {
+            bool isFizz = IsFizz(number);
+            bool isBuzz = IsBuzz(number);
+
             if (number % 15 == 0 || (ContainsDigit(number, 5) && ContainsDigit(number, 3)))
             {
                 return "fizz buzz";
@@ -20,6 +23,16 @@
             }
 
             return number.ToString();
+        }
+
+        private static bool IsFizz(int num)
+        {
+            return num % 3 == 0 || ContainsDigit(num, 3);
+        }
+
+        private static bool IsBuzz(int num)
+        {
+            return num % 5 == 0 || ContainsDigit(num, 5);
         }
 
         private static bool ContainsDigit(int num, int numToCheck)
