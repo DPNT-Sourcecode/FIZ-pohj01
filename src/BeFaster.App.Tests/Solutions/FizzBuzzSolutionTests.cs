8335249a8a025b5c95976c6e6c985885aca27dbb
sbx_user1060 <sbx_user1060@ip-10-11-203-51.eu-west-2.compute.internal>
2018-04-27 12:21:22 +0000

    Fri Apr 27 12:21:22 UTC 2018

---
+++ src/BeFaster.App.Tests/Solutions/FizzBuzzSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeFaster.App.Solutions;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions
+{
+    [TestFixture]
+    public class FizzBuzzSolutionTests
+    {
+        [TestCase(5, ExpectedResult = "buzz")]
+        [TestCase(3, ExpectedResult = "fizz")]
+        [TestCase(15, ExpectedResult = "fizz buzz")]
+        public void FizzBuzzTest(int num)
+        {
+            FizzBuzzSolution.FizzBuzz(num);
+        }
+    }
+}
