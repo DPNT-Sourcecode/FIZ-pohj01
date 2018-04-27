a4f47062bea2758fbba5a35379d63b6820fbec7b
sbx_user1060 <sbx_user1060@ip-10-11-203-51.eu-west-2.compute.internal>
2018-04-27 13:20:41 +0000

    Fri Apr 27 13:20:41 UTC 2018

--- src/BeFaster.App.Tests/Solutions/FizzBuzzSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/FizzBuzzSolutionTests.cs
@@ -48,23 +48,29 @@ namespace BeFaster.App.Tests.Solutions
             return FizzBuzzSolution.FizzBuzz(num);
         }
 
-        [TestCase(1, ExpectedResult = false)]
-        [TestCase(10, ExpectedResult = false)]
-        [TestCase(11, ExpectedResult = true)]
-        [TestCase(55, ExpectedResult = true)]
-        [TestCase(57, ExpectedResult = false)]
-        [TestCase(60, ExpectedResult = false)]
-        [TestCase(100, ExpectedResult = false)]
-        [TestCase(111, ExpectedResult = true)]
-        [TestCase(999, ExpectedResult = true)]
-        [TestCase(979, ExpectedResult = false)]
-        [TestCase(1999, ExpectedResult = false)]
-        [TestCase(9991, ExpectedResult = false)]
-        [TestCase(9999, ExpectedResult = true)]
+
+        [TestCase(11, ExpectedResult = DeluxType.Fake)]
+        [TestCase(22, ExpectedResult = DeluxType.Normal)]
+        [TestCase(55, ExpectedResult = DeluxType.Fake)]
+        [TestCase(111, ExpectedResult = DeluxType.Fake)]
+        [TestCase(222, ExpectedResult = DeluxType.Normal)]
+        [TestCase(999, ExpectedResult = DeluxType.Fake)]
+        [TestCase(5555, ExpectedResult = DeluxType.Fake)]
+        [TestCase(6666, ExpectedResult = DeluxType.Normal)]
+        [TestCase(9999, ExpectedResult = DeluxType.Fake)]
         public bool FizzBuzzTest_IsDeluxe(int num)
         {
             DeluxType? deluxType;
             return FizzBuzzSolution.IsDeluxe(num, out deluxType);
+        }
+
+        [TestCase(10, ExpectedResult = false)]
+        public bool FizzBuzzTest_IsDeluxe_DeluxType(int num)
+        {
+            DeluxType? deluxType;
+            FizzBuzzSolution.IsDeluxe(num, out deluxType);
+
+            return deluxType.Value;
         }
     }
 }
