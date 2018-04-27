70fbe0c810bcbce1930d7085b6283b48757f6c9d
sbx_user1059 <sbx_user1059@ip-10-12-7-208.eu-west-2.compute.internal>
2018-04-27 10:30:49 +0000

    Fri Apr 27 10:30:49 UTC 2018

--- src/BeFaster.App/Solutions/SumSolution.cs
+++ src/BeFaster.App/Solutions/SumSolution.cs
@@ -1,4 +1,4 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System;
 
 namespace BeFaster.App.Solutions
 {
@@ -6,7 +6,24 @@ namespace BeFaster.App.Solutions
     {
         public static int Sum(int x, int y)
         {
+            if (isValidParameter(x) == false)
+            {
+                Console.WriteLine("x should be in range from 0 to 100");
+                return -1;
+            }
+
+            if (isValidParameter(y) == false)
+            {
+                Console.WriteLine("y should be in range from 0 to 100");
+                return -1;
+            }
+
             return x + y;
+        }
+
+        private static bool isValidParameter(int parameter)
+        {
+            return (parameter >= 0 && parameter <= 100);
         }
     }
 }
